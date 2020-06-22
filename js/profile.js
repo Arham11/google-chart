b25f8f75018923a12acbfaefb9dbd1571922ec60
Arham Chowdhry <arhamchowdhry@gmail.com>
2020-06-22 12:48:00 +0530

    git

--- js/profile.js
+++ js/profile.js
@@ -25,4 +25,19 @@ $(document).ready(function() {
           //remove showing status
           "info":     false
     });
+    
+    $('input[name="daterange"]').daterangepicker({
+    opens: 'right'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+
+
+
 } );
+// $('#dateArc').daterangepicker({
+//     "startDate": "06/16/2020",
+//     "endDate": "06/22/2020"
+// }, function(start, end, label) {
+//   console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+// });
